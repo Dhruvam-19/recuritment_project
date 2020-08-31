e00b241e61109a800168578cef879994b060dc97
Dhruvam <dhruvam.jhavericore@gmail.com>
2020-08-31 19:41:49 +0530

    Database configration and user register form is added.

--- recruitment/recruitment/settings.py
+++ recruitment/recruitment/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.app.UsersConfig',
+    'crispy_forms',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'recruitment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recruitmentdb',
+        'USER' : 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost'
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK ="bootstrap4"
--- recruitment/users/views.py
+++ recruitment/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import UserCreationForm,Userregisterform
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    return render(request,"users/home.html")
+
+def register(request):
+    if request.method == "POST":
+        form = Userregisterform(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data["username"]
+            messages.success(request,f"{username} account is created")
+            return redirect("home")
+    else:
+        form = Userregisterform()
+    return render(request,"users/register.html",{"form":form})
